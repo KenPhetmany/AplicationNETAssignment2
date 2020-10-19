95e52a0b103c369afe4b8493971beaab2da849af
KenPhetmany <kphetmany@student.uts.edu.au>
2020-10-19 20:28:25 +1100

    MR SUILIMAN

--- TextEditor.cs
+++ TextEditor.cs
@@ -66,7 +66,7 @@ namespace assignment2
                 string path = "documents\\" + fileName;
                 //If the file exists, then it overrwrites the current file and floods it with new changes
                 richTextBox.SaveFile(path, RichTextBoxStreamType.RichText);
-                fileName = Path.GetFileName(openFile.FileName);
+                fileName = Path.GetFileName(saveFile.FileName);
                 toolStripFileName.Text = "File Name: " + fileName;
                 }
             else
@@ -79,7 +79,7 @@ namespace assignment2
                         //File is saved with the user input on the file name
                         richTextBox.SaveFile(saveFile.FileName,
                        RichTextBoxStreamType.RichText);
-                        fileName = Path.GetFileName(openFile.FileName);
+                        fileName = Path.GetFileName(saveFile.FileName);
                         toolStripFileName.Text = "File Name:" + fileName;
                         }
                     catch (Exception ex)
@@ -182,7 +182,7 @@ namespace assignment2
                 //If the file exists, then it overrwrites the current file and floods it with new changes
                 string path = "documents\\" + fileName;
                 richTextBox.SaveFile(path, RichTextBoxStreamType.RichText);
-                fileName = Path.GetFileName(openFile.FileName);
+                fileName = Path.GetFileName(saveFile.FileName);
                 toolStripFileName.Text = "File Name: " + fileName;
                 }
             else
@@ -195,7 +195,7 @@ namespace assignment2
                         //File is saved with the user input on the file name
                         richTextBox.SaveFile(saveFile.FileName,
                        RichTextBoxStreamType.RichText);
-                        fileName = Path.GetFileName(openFile.FileName);
+                        fileName = Path.GetFileName(saveFile.FileName);
                         toolStripFileName.Text = "File Name: " + fileName;
                         }
                     catch (Exception ex)
@@ -229,7 +229,8 @@ namespace assignment2
         //Logs out the user and closes the application
         private void logoutToolStripMenuItem_Click(object sender, EventArgs e)
             {
-            Application.Exit();
+            this.Hide();
+            Login login = new Login();
             }
 
         //Copies a selection of the text
